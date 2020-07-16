0bab6fcecb7e73f3fce5a1686d69bdeafdc9b73a
bhavankatre007 <68184179+bhavankatre007@users.noreply.github.com>
2020-07-16 14:56:36 +0530

    Update add.php

--- week1/add.php
+++ week1/add.php
@@ -1,69 +1,148 @@
 <?php
+
 session_start();
 
-if (!isset($_SESSION['name'])) {
-    die('Not logged in');
+if ( ! isset($_SESSION['name']) ) {
+	die("Not logged in");
 }
 
-require_once "pdo.php";
+// If the user requested logout go back to index.php
+if ( isset($_POST['cancel']) ) {
+    header('Location: index.php');
+    return;
+}
 
-if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email'])
-    && isset($_POST['headline']) && isset($_POST['summary'])) {
-    if (strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1 || strlen($_POST['email']) < 1 ||
-        strlen($_POST['headline']) < 1 || strlen($_POST['summary']) < 1) {
-        $_SESSION['error'] = 'All values are required';
+$status = false;
+
+if ( isset($_SESSION['status']) ) {
+	$status = htmlentities($_SESSION['status']);
+	$status_color = htmlentities($_SESSION['color']);
+
+	unset($_SESSION['status']);
+	unset($_SESSION['color']);
+}
+
+try 
+{
+    $pdo = new PDO("mysql:host=localhost;dbname=coursera_javascript_jquery_and_json", "root", "root");
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+
+$name = htmlentities($_SESSION['name']);
+
+$_SESSION['color'] = 'red';
+
+// Check to see if we have some POST data, if we do process it
+if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['headline']) && isset($_POST['summary'])) 
+{
+    if (strlen($_POST['first_name']) < 1 || strlen($_POST['last_name']) < 1 || strlen($_POST['email']) < 1 || strlen($_POST['headline']) < 1 || strlen($_POST['summary']) < 1)
+    {
+        $_SESSION['status'] = "All fields are required";
         header("Location: add.php");
         return;
-    } else {
-        $stmt = $pdo->prepare('INSERT INTO Profile (user_id,first_name, last_name, email, headline, summary) VALUES (:user_id, :first_name, :last_name, :email, :headline,:summary)');
-        $stmt->execute(array(
-                ':user_id' => $_SESSION['user_id'],
-                ':first_name' => $_POST['first_name'],
-                ':last_name' => $_POST['last_name'],
-                ':email' => $_POST['email'],
-                ':headline' => $_POST['headline'],
-                ':summary' => $_POST['summary'])
-        );
-        $_SESSION['success'] = "Record added.";
-        header("Location: index.php");
+    }
+
+    if (strpos($_POST['email'], '@') === false)
+    {
+        $_SESSION['status'] = "Email address must contain @";
+        header("Location: add.php");
         return;
     }
 
+    $first_name = htmlentities($_POST['first_name']);
+    $last_name = htmlentities($_POST['last_name']);
+    $email = htmlentities($_POST['email']);
+    $headline = htmlentities($_POST['headline']);
+    $summary = htmlentities($_POST['summary']);
+
+    $stmt = $pdo->prepare("
+        INSERT INTO profile (user_id, first_name, last_name, email, headline, summary)
+        VALUES (:user_id, :first_name, :last_name, :email, :headline, :summary)
+    ");
+
+    $stmt->execute([
+        ':user_id' => $_SESSION['user_id'],
+        ':first_name' => $first_name, 
+        ':last_name' => $last_name, 
+        ':email' => $email,
+        ':headline' => $headline,
+        ':summary' => $summary,
+    ]);
+
+    $_SESSION['status'] = 'Record added';
+    $_SESSION['color'] = 'green';
+
+    header('Location: index.php');
+	return;
+    
 }
-else{
-    $_SESSION['fail']="All values are required";
-}?>
+
+?>
 <!DOCTYPE html>
 <html>
-<head>
-    <?php require_once "bootstrap.php"; ?>
-    <title>Ziang Cui 's Login Page</title>
-</head>
-<body>
-<div class="container">
-    <h1>Adding Profile for UMSI</h1>
-    <?php
-    if (isset($_SESSION['fail'])) {
-        echo('<p style="color: red;">' . htmlentities($_SESSION['fail']) . "</p>\n");
-        unset($_SESSION['fail']);
-    }
-    ?>
-    <form method="post">
-        <p>First Name:
-            <input type="text" name="first_name" size="60"/></p>
-        <p>Last Name:
-            <input type="text" name="last_name" size="60"/></p>
-        <p>Email:
-            <input type="text" name="email" size="30"/></p>
-        <p>Headline:<br/>
-            <input type="text" name="headline" size="80"/></p>
-        <p>Summary:<br/>
-            <textarea name="summary" rows="8" cols="80"></textarea>
-        <p>
-            <input type="submit" value="Add">
-            <input type="submit" name="cancel" value="Cancel">
-        </p>
-    </form>
-</div>
-</body>
+    <head>
+        <title>Ivan Neradovic Autos</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Adding Profile for <?php echo $name; ?></h1>
+            <?php
+                if ( $status !== false ) 
+                {
+                    // Look closely at the use of single and double quotes
+                    echo(
+                        '<p style="color: ' .$status_color. ';" class="col-sm-10 col-sm-offset-2">'.
+                            htmlentities($status).
+                        "</p>\n"
+                    );
+                }
+            ?>
+            <form method="post" class="form-horizontal">
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="first_name">First Name:</label>
+                    <div class="col-sm-5">
+                        <input class="form-control" type="text" name="first_name" id="first_name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="last_name">Last Name:</label>
+                    <div class="col-sm-5">
+                        <input class="form-control" type="text" name="last_name" id="last_name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="email">Email:</label>
+                    <div class="col-sm-5">
+                        <input class="form-control" type="text" name="email" id="email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="headline">Headline:</label>
+                    <div class="col-sm-5">
+                        <input class="form-control" type="text" name="headline" id="headline">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="summary">Summary:</label>
+                    <div class="col-sm-5">
+                        <textarea class="form-control" name="summary" id="summary" rows="8"></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-2 col-sm-offset-2">
+                        <input class="btn btn-primary" type="submit" value="Add">
+                        <input class="btn" type="submit" name="cancel" value="Cancel">
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </body>
 </html>
